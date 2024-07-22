1a4c4632e2cedf6ca6588f5b4d786f22e9052581
krishna <krishh16@yahoo.com>
2024-07-22 14:13:33 +0000

    added required file structure"

---
+++ Fund me/script/Counter.s.sol
@@ -0,0 +1,15 @@
+//SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
+
+contract DeployCounter is Script {
+    function run() external returns (Counter) {
+        vm.startBroadcast();
+        Counter counter = new Counter();
+        vm.stopBroadcast();
+        return counter;
+    }
+}
