55f930560f5bea4f15ae315592aebf4abf3c8243
krishna <krishh16@yahoo.com>
2024-07-23 17:10:48 +0000

    added a bunch of tests

--- Fund me/test/FundMeTests.t.sol
+++ Fund me/test/FundMeTests.t.sol
@@ -8,10 +8,16 @@ import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test{
     FundMe fundMe;
+   
+    address User = makeAddr("user");
+    uint256 constant STARTING_BALANCE = 10 ether;
     function setUp() external{
         DeployFundMe deployFundMe = new DeployFundMe();
         fundMe = deployFundMe.run();
+        vm.deal(User,STARTING_BALANCE );
     }
+
+
 
     function testMinimumDollarIsFive() view public{
         assertEq(fundMe.MINIMUM_USD(), 5e18);
@@ -24,5 +30,17 @@ contract FundMeTest is Test{
     function testPriceFeedVersionIsAccurate()view public{
         uint version = fundMe.getVersion();
         assertEq(version,4);
+    }
+
+    function testExpectedRevert()  public {
+        vm.expectRevert();
+        fundMe.withdraw();
+    }
+
+    function testWhetherTheUserHasEnoughEth() public {
+        vm.prank(User); //sends the transaction as address User
+        fundMe.fund{value: 0.1 ether}();
+
+        assertEq(fundMe.getAddressToAmount(User), 0.1 ether);
     }
 }
