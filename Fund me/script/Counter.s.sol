adc006e4f8b4aa0ff8107fdc8487019d010e46f5
krishna <krishh16@yahoo.com>
2024-07-23 12:10:01 +0000

    added contract code

---
+++ Fund me/script/Counter.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {Counter} from "../src/Counter.sol";
+
+contract DeployScript is Script {
+    function run() external {
+        
+    }
+}
