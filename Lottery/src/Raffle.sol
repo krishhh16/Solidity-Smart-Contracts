bb6b41520c29221ff37df3ed14d673c960074678
krishna <krishh16@yahoo.com>
2024-07-25 11:59:14 +0000

    added vrf keeper functionality to automatically call the prize winner function periodically

--- Lottery/src/Raffle.sol
+++ Lottery/src/Raffle.sol
@@ -24,6 +24,7 @@ pragma solidity 0.8.19;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title Raffle Contract
  * @author Zeref
@@ -37,6 +38,7 @@ contract RaffleContract is VRFConsumerBaseV2Plus {
     error Raffle__TransactionFailed();
     error RaffleNotOpen();
     error RaffleHasNotEnded();
+    error Raffle_UpkeepNotNeeded(uint balance, uint participantsLength, RaffleIsOpen raffleIsOpen);
 
     /*Type Declaration*/
     enum RaffleIsOpen {
@@ -44,13 +46,12 @@ contract RaffleContract is VRFConsumerBaseV2Plus {
         CALCULATING
     }
 
-
     /*State Variables */
     uint immutable s_entranceFee;
     address payable[] s_participants;
     // @dev duration of lottery in seconds
     uint immutable i_interval;
-    uint immutable s_lastTimestamp;
+    uint s_lastTimestamp;
     uint16 private constant REQUEST_CONFIRMATION = 3;
     uint32 private constant NUM_WORDS = 1;
     bytes32 private immutable i_keyHash;
@@ -59,28 +60,31 @@ contract RaffleContract is VRFConsumerBaseV2Plus {
     address payable mostRecentWinner;
     RaffleIsOpen raffleOpen;
 
+    /*Events*/
+
     event RaffleEntered();
+    event WinnerPicked(address indexed winner);
 
     constructor(
         uint _entranceFee,
-        uint interval, 
+        uint interval,
         address _vrfCoordinator,
         uint subscriptionId,
         uint32 callbackGasLimit,
         bytes32 keyHash
-        ) VRFConsumerBaseV2Plus(_vrfCoordinator) {
+    ) VRFConsumerBaseV2Plus(_vrfCoordinator) {
         i_interval = interval;
         i_subscriptionId = subscriptionId;
         i_keyHash = keyHash;
         i_callbackGasLimit = callbackGasLimit;
-        
+
         s_entranceFee = _entranceFee;
         s_lastTimestamp = block.timestamp;
         raffleOpen = RaffleIsOpen.OPEN;
     }
 
     function enterRaffle() external payable {
-        if(msg.value < s_entranceFee) {
+        if (msg.value < s_entranceFee) {
             revert Raffle__NotEnoughCashStranger();
         }
         s_participants.push(payable(msg.sender));
@@ -88,15 +92,26 @@ contract RaffleContract is VRFConsumerBaseV2Plus {
         emit RaffleEntered();
     }
 
-    function prizeWinner() external {
-        if((block.timestamp - s_lastTimestamp) < i_interval ){
-            revert RaffleHasNotEnded();
-        }
-        if (raffleOpen != RaffleIsOpen.OPEN ){
-            revert RaffleNotOpen();
+    function checkUpkeep(
+        bytes memory
+    ) public view returns (bool upkeepNeeded, bytes memory) {
+        bool timeHasPassed = ((block.timestamp - s_lastTimestamp) >= i_interval);
+        bool isOpen = raffleOpen == RaffleIsOpen.OPEN;
+        bool participantsExists = s_participants.length > 0;
+        bool hasBalance = address(this).balance > 0;
+
+        upkeepNeeded = timeHasPassed && isOpen && participantsExists && hasBalance;
+        return (upkeepNeeded, "");
+    }
+
+    function performUpkeep(bytes calldata) external {
+        (bool upkeepNeeded,) = checkUpkeep("");
+        if(!upkeepNeeded) {
+            revert Raffle_UpkeepNotNeeded(address(this).balance, s_participants.length, raffleOpen);
         }
 
-        uint requestId = s_vrfCoordinator.requestRandomWords(
+        raffleOpen = RaffleIsOpen.CALCULATING;
+        s_vrfCoordinator.requestRandomWords(
             VRFV2PlusClient.RandomWordsRequest({
                 keyHash: i_keyHash,
                 subId: i_subscriptionId,
@@ -109,29 +124,58 @@ contract RaffleContract is VRFConsumerBaseV2Plus {
                 )
             })
         );
-        raffleOpen = RaffleIsOpen.CALCULATING;
+   }
 
+    function prizeWinner() external {
+        if ((block.timestamp - s_lastTimestamp) < i_interval) {
+            revert RaffleHasNotEnded();
+        }
+        if (raffleOpen != RaffleIsOpen.OPEN) {
+            revert RaffleNotOpen();
+        }
+
+        raffleOpen = RaffleIsOpen.CALCULATING;
+        s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATION,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
     }
 
-     function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override{
+    function fulfillRandomWords(
+        uint256, //requestId,
+        uint256[] calldata randomWords
+    ) internal override {
         // Pick a random number between 0 and the length of the participants array
-        address payable recentWinner = s_participants[randomWords[0] % s_participants.length];
+        address payable recentWinner = s_participants[
+            randomWords[0] % s_participants.length
+        ];
         mostRecentWinner = recentWinner;
 
         raffleOpen = RaffleIsOpen.OPEN;
-        (bool success,) = recentWinner.call{value: address(this).balance}("");
+        s_participants = new address payable[](0);
+        s_lastTimestamp = block.timestamp;
 
-        if(!success) {
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+
+        if (!success) {
             revert Raffle__TransactionFailed();
-        } 
+        }
+        emit WinnerPicked(mostRecentWinner);
+    }
 
-     }
     /*
      * Getter functions are defined from this line forward
      */
-    function getEntranceFee() external view returns (uint){
+    function getEntranceFee() external view returns (uint) {
         return s_entranceFee;
     }
-
-
 }
