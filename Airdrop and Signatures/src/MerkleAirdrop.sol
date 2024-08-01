af9ca3899863aff1c17bbe3a63afdc94f4e49122
krishna <krishh16@yahoo.com>
2024-08-01 19:27:09 +0000

    added proof creation scripts for tests

--- Airdrop and Signatures/src/MerkleAirdrop.sol
+++ Airdrop and Signatures/src/MerkleAirdrop.sol
@@ -6,11 +6,14 @@ import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProo
 contract AirDrop {
     using SafeERC20 for IERC20;
     error AirDrop__FailedToVerify();
+    error AirDrop__AlreadyClaimed();
 
     event ClaimToken(address to, uint amount);
 
     bytes32 immutable  i_merkleRoot;
     IERC20 private immutable i_airdropToken;
+
+    mapping(address claimer => bool hasClaimed) private s_hasCalimed;
 
     constructor(bytes32 _merkleRoot, IERC20 _airdropToken) {
         i_merkleRoot = _merkleRoot;
@@ -18,12 +21,15 @@ contract AirDrop {
     }
 
     function claim(address _account, uint _amount, bytes32[] memory merkleProof ) external {
+        if (!s_hasCalimed[_account]) {
+            revert AirDrop__AlreadyClaimed();
+        }
         bytes32 leaf = keccak256(abi.encode(keccak256(abi.encode(_account, _amount))));
 
         if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)){
             revert AirDrop__FailedToVerify();
         }
-
+        s_hasCalimed[_account] = true;
         emit ClaimToken(_account, _amount);
         i_airdropToken.safeTransfer(_account, _amount);
     }
