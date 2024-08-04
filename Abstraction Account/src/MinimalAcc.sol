407652bcb2688a486dfb04afbad19214c768954f
krishna <krishh16@yahoo.com>
2024-08-04 10:32:17 +0000

    added new contract for account abstraction

---
+++ Abstraction Account/src/MinimalAcc.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+
+contract MinimalAccount is IAccount {
+    function validateUserOp(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash,
+        uint256 missingAccountFunds
+    ) external returns (uint256 validationData){
+        
+    }
+}
